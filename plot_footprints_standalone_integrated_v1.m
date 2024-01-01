92208eb54c690f1fb49803816f5b41f06e07a840
ppiantad <ptpiantadosi@smcm.edu>
2024-01-01 17:07:10 -0500

    updated FOV code

--- plot_footprints_standalone_integrated_v1.m
+++ plot_footprints_standalone_integrated_v1.m
@@ -11,6 +11,7 @@ targetAnimal = 'BLA_Insc_27';
 animal_index_to_plot = find(strcmp(animalIDs, targetAnimal));
 
 
+
 Coor = final.(targetAnimal).(session_to_analyze).neuron.Coor;  
 
 activated = respClass_mouse.(targetAnimal).(session_to_analyze).(epoc_to_align).(identity_classification_str).(filter_args).activated;
@@ -199,3 +200,169 @@ ylabel('Within-Identity Distances Mean');
 colorbar;
 
 hold off;
+
+%% gathering ALL cells
+animalIDs = (fieldnames(final));
+Coor_all_mice = {};
+for qq = 1:size(fieldnames(final),1)
+    animal = animalIDs{qq};
+    Coor_indiv = final.(animal).(session_to_analyze).neuron.Coor;
+    Coor_all_mice = [Coor_all_mice; Coor_indiv ];
+end
+
+%% plot ALL cells
+figure;
+hold on;
+
+
+
+for i = 1:numel(Coor_all_mice)
+    % Get the coordinates of the current circle
+    circleCoords = Coor_all_mice{i};
+    
+    % Extract x and y coordinates
+    y = circleCoords(1, :);
+    x = circleCoords(2, :);
+
+    % Calculate centroid
+    centroid = [mean(x), mean(y)];
+
+    % Store centroid in the array
+    centroids(i, :) = round(centroid);
+    
+    % currently need to load a mouse's data, and select these filters etc.
+    % manually. can probably integrate with the
+    % eventRelatedActivitAndClassification script to make it more automatic
+    if evalin( 'base', 'exist(''respClass_mouse'', ''var'') == 1')
+        if respClass_all_array{1, 1}(1, i) == 1
+            plot_color = "red";
+        elseif respClass_all_array{1, 1}(1, i) == 2
+            plot_color = "blue";
+        elseif respClass_all_array{1, 1}(1, i) == 3
+            plot_color = "black";
+        end
+    else
+    end
+    % Plot the circle
+    plot(x, y, 'Color', plot_color);
+end
+
+hold off;
+
+% Add labels and title
+xlabel('X');
+ylabel('Y');
+% title('Plot of Circles');
+
+% Adjust the aspect ratio if needed
+axis equal;
+
+
+%% withinIdentity for ALL cells
+% Assuming you already have centroids calculated and stored in 'centroids'
+numCategories = max(respClass_all_array{1, 1});
+
+% Initialize arrays to store distances
+% withinIdentityDistances = zeros(size(centroids, 1), size(centroids, 1), numCategories); % 3rd dimension is for Identity categories
+% acrossIdentityDistances = zeros(size(centroids, 1), size(centroids, 1), numCategories);
+neuron = 0;
+% Loop through each identity category for within-identity distances
+for identityCategory = 1:numCategories
+    % Find indices of points in the current identity category
+    withinIdentityIndices = find(respClass_all_array{1, 1} == identityCategory);
+    
+    % Extract coordinates of points within the current identity category
+    withinIdentityCoords = centroids(withinIdentityIndices, :);
+    withinIdentityIndices_array(:, identityCategory) = {withinIdentityIndices};
+    for tt = 1:size(withinIdentityCoords, 1)
+        neuron = neuron + 1;
+        for qq = 1:size(withinIdentityCoords, 1)
+            % Calculate pairwise distances within the current identity category
+            withinIdentityDistances(tt, qq) = pdist([centroids(tt, :); centroids(qq,:)], 'euclidean');
+            withinIdentityDistances_array(:, identityCategory) = {withinIdentityDistances}; 
+            withinIdentityDistances_mean(neuron) = mean(withinIdentityDistances(tt, qq));
+            % DistanceMatrix(k, kk) = pdist([Centroids(k, :); Centroids(kk, :)], 'euclidean'); 
+        end
+    end
+    clear withinIdentityDistances withinIdentityIndices withinIdentityCoords
+end
+
+%% acrossIdentity for ALL cells
+% Assuming you already have centroids calculated and stored in 'centroids'
+numNeurons = size(centroids, 1);
+
+
+% Loop through each neuron
+for neuron = 1:numNeurons
+    % Get the Identity value for the current neuron
+    currentIdentity = respClass_all_array{1, 1}(neuron);
+    
+    % Find indices of neurons with different Identity values
+    otherIdentityIndices = find(respClass_all_array{1, 1} ~= currentIdentity)';
+    
+
+    % add the index for the neuron so that there is a 0 in the array (same
+    % as withinIdentityDistances - can always filter out the non-zero
+    % values later when calculating the mean! 
+    otherIdentityIndices = [neuron; otherIdentityIndices];
+
+    % Extract coordinates of the current neuron
+    currentNeuronCoords = centroids(neuron, :);
+    
+    % Extract coordinates of neurons with different Identity values
+    otherIdentityCoords = centroids(otherIdentityIndices, :);
+    
+    % Calculate pairwise distances between the current neuron and neurons with different Identity values
+    % acrossIdentityDistancesPerNeuron(neuron, :) = pdist2(currentNeuronCoords, otherIdentityCoords);
+    acrossIdentityDistances_array(:, neuron) = {pdist2(currentNeuronCoords, otherIdentityCoords,'euclidean')}; 
+    acrossIdentityDistances_mean(neuron) = mean(acrossIdentityDistances_array{1, neuron});
+end
+
+%%
+% Assuming you have two arrays: withinIdentityDistances_mean and acrossIdentityDistances_mean
+% Also, assuming you have an array called Identity
+% Scatter plot
+
+% Scatter plot
+figure;
+scatter(acrossIdentityDistances_mean, withinIdentityDistances_mean, [], respClass_all_array{1, 1}, 'filled');
+hold on;
+
+% Plotting the diagonal line
+plot([min(acrossIdentityDistances_mean), max(acrossIdentityDistances_mean)], [min(withinIdentityDistances_mean), max(withinIdentityDistances_mean)], '--k');  % Dotted line
+
+colormap('jet');  % You can use a different colormap if desired
+title('Scatter Plot of Distances');
+xlabel('Across-Identity Distances Mean');
+ylabel('Within-Identity Distances Mean');
+colorbar;
+
+hold off;
+
+%%
+% Assuming you have two arrays: withinIdentityDistances_mean and acrossIdentityDistances_mean
+
+% Calculate cumulative distribution functions (CDF)
+[unique_within, ~, ic_within] = unique(withinIdentityDistances_mean);
+cdf_within = cumsum(hist(ic_within, 1:max(ic_within)) / numel(withinIdentityDistances_mean));
+
+[unique_across, ~, ic_across] = unique(acrossIdentityDistances_mean);
+cdf_across = cumsum(hist(ic_across, 1:max(ic_across)) / numel(acrossIdentityDistances_mean));
+
+% Combine the distances for the entire population
+allDistances = [withinIdentityDistances_mean, acrossIdentityDistances_mean];
+[unique_all, ~, ic_all] = unique(allDistances);
+cdf_all = cumsum(hist(ic_all, 1:max(ic_all)) / numel(allDistances));
+
+% Plot cumulative probability distributions
+figure;
+plot(unique_within, cdf_within, 'LineWidth', 2, 'DisplayName', 'Within Identity');
+hold on;
+plot(unique_across, cdf_across, 'LineWidth', 2, 'DisplayName', 'Across Identity');
+plot(unique_all, cdf_all, 'LineWidth', 2, 'DisplayName', 'Entire Population');
+
+title('Cumulative Probability Distribution of Mean Pairwise Distances');
+xlabel('Mean Pairwise Distance');
+ylabel('Cumulative Probability');
+legend('Location', 'best');
+grid on;
