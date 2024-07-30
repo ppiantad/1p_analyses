b0b994d2ff7e8fc136fc839d0cba2cdd8f809eab
Patrick Piantadosi <patrick.piantadosi@nih.gov>
2024-07-30 11:43:44 -0400

    updates

--- block_wise_changes_v1.m
+++ block_wise_changes_v1.m
@@ -50,60 +50,60 @@ sum(block_2_and_3_pre_and_consumption)
 
 %%
 arrays_to_examine = [1 8];
-collect_conserved = prechoice_block_1 == event_for_figures & prechoice_blocks_2_and_3 == event_for_figures;
-collect_conserved_sum = sum(collect_conserved)
+conserved = prechoice_block_1 == event_for_figures & prechoice_blocks_2_and_3 == event_for_figures;
+conserved_sum = sum(conserved)
 
-collect_lost = prechoice_block_1 == event_for_figures & prechoice_blocks_2_and_3 ~= event_for_figures;
-collect_lost_sum = sum(collect_lost)
+lost = prechoice_block_1 == event_for_figures & prechoice_blocks_2_and_3 ~= event_for_figures;
+lost_sum = sum(lost)
 
-collect_remapped = prechoice_block_1 ~= event_for_figures & prechoice_blocks_2_and_3 == event_for_figures;
-collect_remapped_sum = sum(collect_remapped)
+remapped = prechoice_block_1 ~= event_for_figures & prechoice_blocks_2_and_3 == event_for_figures;
+remapped_sum = sum(remapped)
 
 vars_to_use = {'prechoice_block_1', 'prechoice_blocks_2_and_3'};
 
 %%
 arrays_to_examine = [2 9];
-collect_conserved = postchoice_reward_block_1 == event_for_figures & postchoice_reward_blocks_2_and_3 == event_for_figures;
-collect_conserved_sum = sum(collect_conserved)
+conserved = postchoice_reward_block_1 == event_for_figures & postchoice_reward_blocks_2_and_3 == event_for_figures;
+conserved_sum = sum(conserved)
 
-collect_lost = postchoice_reward_block_1 == event_for_figures & postchoice_reward_blocks_2_and_3 ~= event_for_figures;
-collect_lost_sum = sum(collect_lost)
+lost = postchoice_reward_block_1 == event_for_figures & postchoice_reward_blocks_2_and_3 ~= event_for_figures;
+lost_sum = sum(lost)
 
-collect_remapped = postchoice_reward_block_1 ~= event_for_figures & postchoice_reward_blocks_2_and_3 == event_for_figures;
-collect_remapped_sum = sum(collect_remapped)
+remapped = postchoice_reward_block_1 ~= event_for_figures & postchoice_reward_blocks_2_and_3 == event_for_figures;
+remapped_sum = sum(remapped)
 
 vars_to_use = {'postchoice_reward_block_1', 'postchoice_reward_blocks_2_and_3'};
 
 %%
 arrays_to_examine = [3 10];
-collect_conserved = collect_block_1 == event_for_figures & collect_blocks_2_and_3 == event_for_figures;
-collect_conserved_sum = sum(collect_conserved)
+conserved = collect_block_1 == event_for_figures & collect_blocks_2_and_3 == event_for_figures;
+conserved_sum = sum(conserved)
 
-collect_lost = collect_block_1 == event_for_figures & collect_blocks_2_and_3 ~= event_for_figures;
-collect_lost_sum = sum(collect_lost)
+lost = collect_block_1 == event_for_figures & collect_blocks_2_and_3 ~= event_for_figures;
+lost_sum = sum(lost)
 
-collect_remapped = collect_block_1 ~= event_for_figures & collect_blocks_2_and_3 == event_for_figures;
-collect_remapped_sum = sum(collect_remapped)
+remapped = collect_block_1 ~= event_for_figures & collect_blocks_2_and_3 == event_for_figures;
+remapped_sum = sum(remapped)
 
 vars_to_use = {'collect_block_1', 'collect_blocks_2_and_3'};
 
 %% DO NOT USE IF USING CODE ABOVE
 
 collect_all_possible = sum([sum(respClass_all_array{1, arrays_to_examine(1)} ==1), sum(respClass_all_array{1, arrays_to_examine(2)} ==1)])
-collect_conserved = respClass_all_array{1, arrays_to_examine(1)} == event_for_figures & respClass_all_array{1, arrays_to_examine(2)} == event_for_figures;
-collect_conserved_sum = sum(collect_conserved)
+conserved = respClass_all_array{1, arrays_to_examine(1)} == event_for_figures & respClass_all_array{1, arrays_to_examine(2)} == event_for_figures;
+conserved_sum = sum(conserved)
 collect_original_activated_sum = sum(respClass_all_array{1, arrays_to_examine(1)}==1)
 collect_original_inhibited_sum = sum(respClass_all_array{1, arrays_to_examine(1)}==2)
-collect_remapped = respClass_all_array{1, arrays_to_examine(1)} ~= event_for_figures & respClass_all_array{1, arrays_to_examine(2)} == event_for_figures;
-collect_remapped_sum = sum(collect_remapped)
+remapped = respClass_all_array{1, arrays_to_examine(1)} ~= event_for_figures & respClass_all_array{1, arrays_to_examine(2)} == event_for_figures;
+remapped_sum = sum(remapped)
 not_collect_original_sum = sum(respClass_all_array{1, arrays_to_examine(1)} == 3)
 
-collect_lost = respClass_all_array{1, arrays_to_examine(1)} == event_for_figures & respClass_all_array{1, arrays_to_examine(2)} ~= event_for_figures;
-collect_lost_sum = sum(collect_lost)
+lost = respClass_all_array{1, arrays_to_examine(1)} == event_for_figures & respClass_all_array{1, arrays_to_examine(2)} ~= event_for_figures;
+lost_sum = sum(lost)
 collect_all_possible = sum([sum(respClass_all_array{1, arrays_to_examine(1)} == 1), sum(respClass_all_array{1, arrays_to_examine(2)} ==1)])
-collect_conserved_percent = collect_conserved_sum/neuron_num
-collect_remapped_percent = collect_remapped_sum/neuron_num
-collect_lost_percent = collect_lost_sum/neuron_num
+collect_conserved_percent = conserved_sum/neuron_num
+collect_remapped_percent = remapped_sum/neuron_num
+collect_lost_percent = lost_sum/neuron_num
 collect_new_activated_sum = sum(respClass_all_array{1, arrays_to_examine(2)}==1)
 collect_new_inhibited_sum = sum(respClass_all_array{1, arrays_to_examine(2)}==2)
 not_collect_new_sum = sum(respClass_all_array{1, arrays_to_examine(2)} ==3)
@@ -155,13 +155,13 @@ xline(0);
 xlabel('Time from choice (s)');
 
 
-inner_pie = [collect_conserved_sum/neuron_num,...
+inner_pie = [conserved_sum/neuron_num,...
             
-            collect_lost_sum/neuron_num,...
+            lost_sum/neuron_num,...
 
-            collect_remapped_sum/neuron_num,...
+            remapped_sum/neuron_num,...
            
-            (neuron_num- [collect_conserved_sum+collect_remapped_sum+collect_lost_sum]) / neuron_num];
+            (neuron_num- [conserved_sum+remapped_sum+lost_sum]) / neuron_num];
 
 
 labels = ["conserved", "lost", "new (remapped)",  "other"];
@@ -176,11 +176,11 @@ set(gcf, 'Position', [100, 100, width, height]); % Set position and size [left, 
 ytickformat('%.2f');
 N = 5;
 axes('ColorOrder',brewermap(N,'Pastel2'),'NextPlot','replacechildren')
-plot(ts1, mean(neuron_mean_array{1, 1}(collect_conserved  == 1, :)))
-hold on; plot(ts1, mean(neuron_mean_array{1, arrays_to_examine(1)}(collect_remapped == 1, :)))
-hold on; plot(ts1, mean(neuron_mean_array{1, arrays_to_examine(2)}(collect_remapped == 1, :)))
-hold on; plot(ts1, mean(neuron_mean_array{1, arrays_to_examine(2)}(collect_lost == 1, :)))
-hold on; plot(ts1, mean(neuron_mean_array{1, arrays_to_examine(1)}(collect_lost == 1, :)))
+plot(ts1, mean(neuron_mean_array{1, 1}(conserved  == 1, :)))
+hold on; plot(ts1, mean(neuron_mean_array{1, arrays_to_examine(1)}(remapped == 1, :)))
+hold on; plot(ts1, mean(neuron_mean_array{1, arrays_to_examine(2)}(remapped == 1, :)))
+hold on; plot(ts1, mean(neuron_mean_array{1, arrays_to_examine(2)}(lost == 1, :)))
+hold on; plot(ts1, mean(neuron_mean_array{1, arrays_to_examine(1)}(lost == 1, :)))
 
 
 figure;
@@ -190,9 +190,9 @@ set(gcf, 'Position', [100, 100, width, height]); % Set position and size [left, 
 % xtickformat('%.2f');
 ytickformat('%.2f');
 hold on;
-h(1) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(collect_conserved  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(collect_conserved  ==1, :)), 'lineProps', {'color', acton(1,:)});
-h(2) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(collect_remapped  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(collect_remapped  ==1, :)), 'lineProps', {'color', acton(50,:)});
-h(3) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(2)}(collect_remapped  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(2)}(collect_remapped  ==1, :)), 'lineProps', {'color', acton(150,:)});
+h(1) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(conserved  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(conserved  ==1, :)), 'lineProps', {'color', acton(1,:)});
+h(2) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(remapped  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(remapped  ==1, :)), 'lineProps', {'color', acton(50,:)});
+h(3) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(2)}(remapped  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(2)}(remapped  ==1, :)), 'lineProps', {'color', acton(150,:)});
 % h(4) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,2}(collect_lost  ==1, :)), nanmean(neuron_sem_array{1, 2}(collect_lost  ==1, :)), 'lineProps', {'color', acton(200,:)});
 % h(5) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,1}(collect_lost  ==1, :)), nanmean(neuron_sem_array{1, 1}(collect_lost  ==1, :)), 'lineProps', {'color', acton(250,:)});
 % legend([h(1).mainLine h(2).mainLine h(3).mainLine h(4).mainLine h(5).mainLine], 'conserved', 'pre-remapped (safe blocks)', 'post-remapped (risky blocks)')
@@ -209,8 +209,8 @@ xlabel('Time from Large Rew Choice (s)');
 %%
 figure('Position', [100, 100, 300, 600]); % [left, bottom, width, height]
 hold on;
-h(1) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(collect_conserved  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(collect_conserved  ==1, :)), 'lineProps', {'color', 'r'});
-h(2) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(2)}(collect_conserved  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(2)}(collect_conserved  ==1, :)), 'lineProps', {'color', 'b'});
+h(1) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(conserved  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(conserved  ==1, :)), 'lineProps', {'color', 'r'});
+h(2) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(2)}(conserved  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(2)}(conserved  ==1, :)), 'lineProps', {'color', 'b'});
 legend([h(1).mainLine h(2).mainLine], 'conserved (safe block)', 'conserved (risky blocks)')
 xlim([-8 8]);
 ylim([-0.6 0.7])
@@ -226,7 +226,7 @@ ax1 = nexttile;
 hold on;
 
 % Plot the heatmap
-imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(1)}(collect_conserved  ==1, :))
+imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(1)}(conserved  ==1, :))
 
 % % Apply the custom colormap
 % colormap(custom_colormap);
@@ -238,11 +238,11 @@ clim([-1 1]);
 c = colorbar('eastoutside');
 set(c, 'YTick', clim); % 
 
-ylim([1  size(neuron_mean_array{1,arrays_to_examine(1)}(collect_conserved  ==1), 2)])
+ylim([1  size(neuron_mean_array{1,arrays_to_examine(1)}(conserved  ==1), 2)])
 xlim([-8 8]);
 % Set X-axis ticks
 set(gca, 'XTick', [-8, -4, 0, 4, 8]);
-set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(1)}(collect_conserved  ==1), 2)]);
+set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(1)}(conserved  ==1), 2)]);
 xline(0)
 hold off;
 
@@ -251,7 +251,7 @@ ax2 = nexttile;
 hold on;
 
 % Plot the heatmap
-imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(2)}(collect_conserved  ==1, :))
+imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(2)}(conserved  ==1, :))
 
 % % Apply the custom colormap
 % colormap(custom_colormap);
@@ -263,19 +263,19 @@ clim([-1 1]);
 c = colorbar('eastoutside');
 set(c, 'YTick', clim); % 
 
-ylim([1  size(neuron_mean_array{1,arrays_to_examine(2)}(collect_conserved  ==1), 2)])
+ylim([1  size(neuron_mean_array{1,arrays_to_examine(2)}(conserved  ==1), 2)])
 xlim([-8 8]);
 % Set X-axis ticks
 set(gca, 'XTick', [-8, -4, 0, 4, 8]);
-set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(2)}(collect_conserved  ==1), 2)]);
+set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(2)}(conserved  ==1), 2)]);
 xline(0)
 hold off;
 
 %%
 figure('Position', [100, 100, 300, 600]); % [left, bottom, width, height]
 hold on;
-h(1) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(collect_lost  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(collect_lost  ==1, :)), 'lineProps', {'color', 'r'});
-h(2) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(2)}(collect_lost  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(2)}(collect_lost  ==1, :)), 'lineProps', {'color', 'b'});
+h(1) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(lost  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(lost  ==1, :)), 'lineProps', {'color', 'r'});
+h(2) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(2)}(lost  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(2)}(lost  ==1, :)), 'lineProps', {'color', 'b'});
 legend([h(1).mainLine h(2).mainLine], 'lost (safe block)', 'lost (risky blocks)')
 xlim([-8 8]);
 ylim([-0.6 0.7])
@@ -291,7 +291,7 @@ ax1 = nexttile;
 hold on;
 
 % Plot the heatmap
-imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(1)}(collect_lost  ==1, :))
+imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(1)}(lost  ==1, :))
 
 % % Apply the custom colormap
 % colormap(custom_colormap);
@@ -303,11 +303,11 @@ clim([-1 1]);
 c = colorbar('eastoutside');
 set(c, 'YTick', clim); % 
 
-ylim([1  size(neuron_mean_array{1,arrays_to_examine(1)}(collect_lost  ==1), 2)])
+ylim([1  size(neuron_mean_array{1,arrays_to_examine(1)}(lost  ==1), 2)])
 xlim([-8 8]);
 % Set X-axis ticks
 set(gca, 'XTick', [-8, -4, 0, 4, 8]);
-set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(1)}(collect_lost  ==1), 2)]);
+set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(1)}(lost  ==1), 2)]);
 xline(0)
 hold off;
 
@@ -316,7 +316,7 @@ ax2 = nexttile;
 hold on;
 
 % Plot the heatmap
-imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(2)}(collect_lost  ==1, :))
+imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(2)}(lost  ==1, :))
 
 % % Apply the custom colormap
 % colormap(custom_colormap);
@@ -328,11 +328,11 @@ clim([-1 1]);
 c = colorbar('eastoutside');
 set(c, 'YTick', clim); % 
 
-ylim([1  size(neuron_mean_array{1,arrays_to_examine(2)}(collect_lost  ==1), 2)])
+ylim([1  size(neuron_mean_array{1,arrays_to_examine(2)}(lost  ==1), 2)])
 xlim([-8 8]);
 % Set X-axis ticks
 set(gca, 'XTick', [-8, -4, 0, 4, 8]);
-set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(2)}(collect_lost  ==1), 2)]);
+set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(2)}(lost  ==1), 2)]);
 xline(0)
 hold off;
 
@@ -340,8 +340,8 @@ hold off;
 %%
 figure('Position', [100, 100, 300, 600]); % [left, bottom, width, height]
 hold on;
-h(1) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(collect_remapped  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(collect_remapped  ==1, :)), 'lineProps', {'color', 'r'});
-h(2) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(2)}(collect_remapped  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(2)}(collect_remapped  ==1, :)), 'lineProps', {'color', 'b'});
+h(1) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(1)}(remapped  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(1)}(remapped  ==1, :)), 'lineProps', {'color', 'r'});
+h(2) = shadedErrorBar(ts1, nanmean(neuron_mean_array{1,arrays_to_examine(2)}(remapped  ==1, :)), nanmean(neuron_sem_array{1, arrays_to_examine(2)}(remapped  ==1, :)), 'lineProps', {'color', 'b'});
 legend([h(1).mainLine h(2).mainLine], 'new (safe block)', 'new (risky blocks)')
 xlim([-8 8]);
 ylim([-0.6 0.7])
@@ -357,7 +357,7 @@ ax1 = nexttile;
 hold on;
 
 % Plot the heatmap
-imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(1)}(collect_remapped  ==1, :))
+imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(1)}(remapped  ==1, :))
 
 % % Apply the custom colormap
 % colormap(custom_colormap);
@@ -369,11 +369,11 @@ clim([-1 1]);
 c = colorbar('eastoutside');
 set(c, 'YTick', clim); % 
 
-ylim([1  size(neuron_mean_array{1,arrays_to_examine(1)}(collect_remapped  ==1), 2)])
+ylim([1  size(neuron_mean_array{1,arrays_to_examine(1)}(remapped  ==1), 2)])
 xlim([-8 8]);
 % Set X-axis ticks
 set(gca, 'XTick', [-8, -4, 0, 4, 8]);
-set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(1)}(collect_remapped  ==1), 2)]);
+set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(1)}(remapped  ==1), 2)]);
 xline(0)
 hold off;
 
@@ -382,7 +382,7 @@ ax2 = nexttile;
 hold on;
 
 % Plot the heatmap
-imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(2)}(collect_remapped  ==1, :))
+imagesc(ts1, [], neuron_mean_array{1,arrays_to_examine(2)}(remapped  ==1, :))
 
 % % Apply the custom colormap
 % colormap(custom_colormap);
@@ -394,10 +394,50 @@ clim([-1 1]);
 c = colorbar('eastoutside');
 set(c, 'YTick', clim); % 
 
-ylim([1  size(neuron_mean_array{1,arrays_to_examine(2)}(collect_remapped  ==1), 2)])
+ylim([1  size(neuron_mean_array{1,arrays_to_examine(2)}(remapped  ==1), 2)])
 xlim([-8 8]);
 % Set X-axis ticks
 set(gca, 'XTick', [-8, -4, 0, 4, 8]);
-set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(2)}(collect_remapped  ==1), 2)]);
+set(gca, 'YTick', [1, size(neuron_mean_array{1,arrays_to_examine(2)}(remapped  ==1), 2)]);
 xline(0)
 hold off;
+
+%% MULTIPLEX NEURONS
+%%
+arrays_to_examine = [1 8];
+collect_conserved = block_1_pre_and_post  == event_for_figures & block_2_and_3_pre_and_post == event_for_figures;
+collect_conserved_sum = sum(collect_conserved)
+
+collect_lost = block_1_pre_and_post == event_for_figures & block_2_and_3_pre_and_post ~= event_for_figures;
+collect_lost_sum = sum(collect_lost)
+
+collect_remapped = block_1_pre_and_post ~= event_for_figures & block_2_and_3_pre_and_post == event_for_figures;
+collect_remapped_sum = sum(collect_remapped)
+
+vars_to_use = {'block_1_pre_and_post', 'block_2_and_3_pre_and_post'};
+
+%%
+arrays_to_examine = [2 9];
+collect_conserved = block_1_pre_and_post == event_for_figures & block_2_and_3_pre_and_post == event_for_figures;
+collect_conserved_sum = sum(collect_conserved)
+
+collect_lost = block_1_pre_and_post == event_for_figures & block_2_and_3_pre_and_post ~= event_for_figures;
+collect_lost_sum = sum(collect_lost)
+
+collect_remapped = block_1_pre_and_post ~= event_for_figures & block_2_and_3_pre_and_post == event_for_figures;
+collect_remapped_sum = sum(collect_remapped)
+
+vars_to_use = {'postchoice_reward_block_1', 'postchoice_reward_blocks_2_and_3'};
+
+%%
+arrays_to_examine = [3 10];
+collect_conserved = block_1_pre_and_post == event_for_figures & block_2_and_3_pre_and_post == event_for_figures;
+collect_conserved_sum = sum(collect_conserved)
+
+collect_lost = block_1_pre_and_post == event_for_figures & block_2_and_3_pre_and_post ~= event_for_figures;
+collect_lost_sum = sum(collect_lost)
+
+collect_remapped = block_1_pre_and_post ~= event_for_figures & block_2_and_3_pre_and_post == event_for_figures;
+collect_remapped_sum = sum(collect_remapped)
+
+vars_to_use = {'collect_block_1', 'collect_blocks_2_and_3'};
