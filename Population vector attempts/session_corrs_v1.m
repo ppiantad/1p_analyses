6b0aeb23603d1586d8c4b14e30d5eabc4ffd55c5
ppiantad <ptpiantadosi@smcm.edu>
2024-08-23 23:30:08 -0400

    minor updates

--- Population vector attempts/session_corrs_v1.m
+++ Population vector attempts/session_corrs_v1.m
@@ -4,9 +4,23 @@
 % also you should first run block_wise_changes_v1.m to get the blocktimes
 
 
-neuronTypes = respClass_all_array_mouse{1, 3};
-neuralActivity = final.BLA_Insc_24.RDT_D1.CNMFe_data.C_raw;
-BehavData = final.BLA_Insc_24.RDT_D1.uv.BehavData;
+select_mouse = 'BLA_Insc_35';
+
+% for RDT D1 BLA_Insc_25:
+%prechoice neuron num 46
+%postchoice rew num 38
+%consumption num 39
+%shock num 11
+
+select_mouse_index = find(strcmp(animalIDs, select_mouse));
+
+first_session = 'RDT_D1';
+
+
+neuronTypes = respClass_all_array_mouse{select_mouse_index, 3};
+neuralActivity = final.(select_mouse).(first_session).CNMFe_data.C_raw;
+BehavData = final.(select_mouse).(first_session).uv.BehavData;
+
 
 
 %%
@@ -109,10 +123,10 @@ title('Mean Similarity Across Stages');
 next_neuron = 0;
 
 PV_consumption_mouse = [];
-for ff = 1:size(respClass_all_array_mouse{1, 3}, 2)
-    if respClass_all_array_mouse{1, 3}(ff) == 1
+for ff = 1:size(respClass_all_array_mouse{select_mouse_index, 3}, 2)
+    if respClass_all_array_mouse{select_mouse_index, 3}(ff) == 1
         next_neuron = next_neuron+1;
-        PV_consumption_mouse (next_neuron) = neuron_mean_mouse{1, 3}(ff, 91);
+        PV_consumption_mouse (next_neuron) = neuron_mean_mouse{select_mouse_index, 3}(ff, 91);
 
 
 
@@ -123,11 +137,11 @@ end
 
 
 
-block_1_data_for_consumption_neurons = block_1_ca_mouse{1, 1}(respClass_all_array_mouse{1, 3} == 1, :);
+block_1_data_for_consumption_neurons = block_1_ca_mouse{1, 1}(respClass_all_array_mouse{select_mouse_index, 3} == 1, :);
 
 num_samples = size(block_1_data_for_consumption_neurons, 2)
 % sampling_frequency = (final.(select_mouse).(first_session).choiceTime.uv.dt)*100;
-sampling_frequency = (final.BLA_Insc_24.RDT_D1.uv.dt)*100;
+sampling_frequency = (final.(select_mouse).(first_session).uv.dt)*100;
 % Create a time array
 time_array = (0:(num_samples-1)) / sampling_frequency;
 
@@ -155,17 +169,17 @@ xline(BehavData.choiceTime(BehavData.shock == 1), '--y')
 %%
 
 PV_consumption_all_mouse = [];
-for ff = 1:size(zall_mouse{1, 3}, 2)
-    for cc = 1:size(zall_mouse{1,3}{1, ff}, 1)
-        PV_consumption_all_mouse(cc, ff) = zall_mouse{1,3}{1, ff}(cc, 91);
+for ff = 1:size(zall_mouse{select_mouse_index, 3}, 2)
+    for cc = 1:size(zall_mouse{select_mouse_index,3}{1, ff}, 1)
+        PV_consumption_all_mouse(cc, ff) = mean(zall_mouse{select_mouse_index,3}{1, ff}(cc, ts1 >= 1 & ts1 <= 3));
 
     end
 
 end
 
 
-PV_consumption_all_mouse_just_consumption = PV_consumption_all_mouse(:, respClass_all_array_mouse{1, 3} == 1); 
-PV_consumption_all_mouse_just_consumption = [PV_consumption_all_mouse_just_consumption PV_consumption_all_mouse(:, respClass_all_array_mouse{1, 3} ~= 1)];
+PV_consumption_all_mouse_just_consumption = PV_consumption_all_mouse(:, respClass_all_array_mouse{select_mouse_index, 3} == 1); 
+PV_consumption_all_mouse_just_consumption = [PV_consumption_all_mouse_just_consumption PV_consumption_all_mouse(:, respClass_all_array_mouse{select_mouse_index, 3} ~= 1)];
 mean_PV_consumption_all_mouse = mean(PV_consumption_all_mouse)';
 hold on;
 figure; imagesc(1:size(PV_consumption_all_mouse_just_consumption, 1), [], PV_consumption_all_mouse_just_consumption')
@@ -177,9 +191,9 @@ hold off;
 
 
 
-ca = final.BLA_Insc_24.(session_to_analyze).CNMFe_data.(ca_data_type);
+ca = final.(select_mouse).(session_to_analyze).CNMFe_data.(ca_data_type);
 ca_zscored = zscore(ca, [], 2);
-time_array = final.BLA_Insc_24.(session_to_analyze).time;
+time_array = final.(select_mouse).(first_session).time;
 
 for t = 1:size(ca_zscored, 2)
     % for i = 1:length(uniqueTypes)
@@ -193,7 +207,7 @@ for t = 1:size(ca_zscored, 2)
     % end
 end
 
-figure; plot(time_array(1:size(similarityOverTime, 2)), similarityOverTime)
+figure; plot(time_array, similarityOverTime(1, 1:size(time_array, 1)))
 xline(BehavData.stTime(BehavData.bigSmall == 1.2), '--b')
 xline(BehavData.stTime(BehavData.bigSmall == 0.3), '--g')
 xline(BehavData.choiceTime(BehavData.bigSmall == 1.2 | BehavData.bigSmall == 0.3), '--r')
@@ -202,17 +216,17 @@ xline(BehavData.choiceTime(BehavData.shock == 1), '--y')
 
 %%
 PV_prechoice_all_mouse = [];
-for ff = 1:size(zall_mouse{1, 1}, 2)
-    for cc = 1:size(zall_mouse{1,1}{1, ff}, 1)
-        PV_prechoice_all_mouse(cc, ff) = zall_mouse{1,1}{1, ff}(cc, 71);
+for ff = 1:size(zall_mouse{select_mouse_index, 1}, 2)
+    for cc = 1:size(zall_mouse{select_mouse_index,1}{1, ff}, 1)
+        PV_prechoice_all_mouse(cc, ff) = mean(zall_mouse{select_mouse_index,1}{1, ff}(cc, ts1 >= -4 & ts1 <= 0));
 
     end
 
 end
 
 
-PV_prechoice_all_mouse_just_prechoice = PV_prechoice_all_mouse(:, respClass_all_array_mouse{1, 1} == 1); 
-PV_prechoice_all_mouse_just_prechoice = [PV_prechoice_all_mouse_just_prechoice PV_prechoice_all_mouse(:, respClass_all_array_mouse{1, 1} ~= 1)];
+PV_prechoice_all_mouse_just_prechoice = PV_prechoice_all_mouse(:, respClass_all_array_mouse{select_mouse_index, 1} == 1); 
+PV_prechoice_all_mouse_just_prechoice = [PV_prechoice_all_mouse_just_prechoice PV_prechoice_all_mouse(:, respClass_all_array_mouse{select_mouse_index, 1} ~= 1)];
 mean_PV_prechoice_all_mouse = mean(PV_prechoice_all_mouse)';
 hold on;
 figure; imagesc(1:size(PV_prechoice_all_mouse_just_prechoice, 1), [], PV_prechoice_all_mouse_just_prechoice')
@@ -224,9 +238,9 @@ hold off;
 
 
 
-ca = final.BLA_Insc_24.(session_to_analyze).CNMFe_data.(ca_data_type);
+ca = final.(select_mouse).(first_session).CNMFe_data.(ca_data_type);
 ca_zscored = zscore(ca, [], 2);
-time_array = final.BLA_Insc_24.(session_to_analyze).time;
+time_array = final.(select_mouse).(first_session).time;
 
 for t = 1:size(ca_zscored, 2)
     % for i = 1:length(uniqueTypes)
@@ -240,7 +254,7 @@ for t = 1:size(ca_zscored, 2)
     % end
 end
 
-figure; plot(time_array(1:size(similarityOverTime, 2)), similarityOverTime)
+figure; plot(time_array, similarityOverTime(1, 1:size(time_array, 1)))
 xline(BehavData.stTime(BehavData.bigSmall == 1.2), '--b')
 xline(BehavData.stTime(BehavData.bigSmall == 0.3), '--g')
 xline(BehavData.choiceTime(BehavData.bigSmall == 1.2 | BehavData.bigSmall == 0.3), '--r')
@@ -248,7 +262,7 @@ xline(BehavData.collectionTime(BehavData.bigSmall == 1.2 | BehavData.bigSmall ==
 xline(BehavData.choiceTime(BehavData.shock == 1), '--y')
 
 
-b1_prechoice_corr = mean(similarityOverTime(1, time_array > block_1_mouse(1, 1) & time_array <= block_1_mouse(1, 2)));
-b2_prechoice_corr = mean(similarityOverTime(1, time_array > block_2_mouse(1, 1) & time_array <= block_2_mouse(1, 2)));
-b3_prechoice_corr = mean(similarityOverTime(1, time_array > block_3_mouse(1, 1) & time_array <= block_3_mouse(1, 2)));
+b1_prechoice_corr = mean(similarityOverTime(1, time_array > block_1_mouse(select_mouse_index, 1) & time_array <= block_1_mouse(select_mouse_index, 2)));
+b2_prechoice_corr = mean(similarityOverTime(1, time_array > block_2_mouse(select_mouse_index, 1) & time_array <= block_2_mouse(select_mouse_index, 2)));
+b3_prechoice_corr = mean(similarityOverTime(1, time_array > block_3_mouse(select_mouse_index, 1) & time_array <= block_3_mouse(select_mouse_index, 2)));
 
