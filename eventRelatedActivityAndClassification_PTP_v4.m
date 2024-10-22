f0b04b60a29a62267db2be78948511481fc92c4b
ppiantad <ptpiantadosi@smcm.edu>
2024-10-21 23:23:20 -0400

    updates

--- eventRelatedActivityAndClassification_PTP_v4.m
+++ eventRelatedActivityAndClassification_PTP_v4.m
@@ -55,7 +55,7 @@ uv.ca_data_type = "C_raw"; % C % C_raw %S
 % CNMFe_data.spike_prob: CASCADE inferred spikes - multiply x sampling rate
 % (10) for spike rate
 
-session_to_analyze = 'RDT_D2';
+session_to_analyze = 'Pre_RDT_RM';
 uv.yoke_data = 0; % set to 1 if you want to be prompted to yoke the number of trials analyzed, set to 0 otherwise
 
 epoc_to_align = 'choiceTime'; % stTime choiceTime collectionTime
