df2f5605df610e85480f53d186ccfa930fedbfa2
ppiantad <ptpiantadosi@smcm.edu>
2025-01-06 01:18:58 -0500

    updates

--- context fear/get_context_freeze.m
+++ context fear/get_context_freeze.m
@@ -1,6 +1,6 @@
 animalIDs = fieldnames(final_DLC);
 
-session_to_analyze = 'D3';
+session_to_analyze = 'D4';
 
 recorded_fps = 30;
 
@@ -36,7 +36,8 @@ end
 for i = 1:total_stimuli
     stimulus_frames{i} = stimulus_times{i} * recorded_fps;
 end
-
+% experimental_grps = readtable('E:\MATLAB\my_repo\context fear\organize_SLEAP_data\full_pilot_mice.xlsx');
+experimental_grps = readtable('I:\MATLAB\my_repo\context fear\organize_DLC_data\pilot groups.xlsx');
 
 
 %%
@@ -47,7 +48,7 @@ mouse_count = 0;
 for gg = 1:size(animalIDs, 1)
     current_mouse = animalIDs{gg};
     
-    if strcmp(final_DLC.(current_mouse).experimental_grp, 'No Shock')
+    if strcmp(final_DLC.(current_mouse).experimental_grp, 'Experimental')
         mouse_count = mouse_count+1;
         mouse_in_cond(mouse_count, :) = current_mouse;
         mouse_data = final_DLC.(current_mouse).(session_to_analyze);
@@ -277,6 +278,7 @@ for gg = 1:size(animalIDs, 1)
             row_idx = strcmp(experimental_grps.mouse, current_mouse);
 
             freeze_data(mouse_count, :) = labels(1:21590)';
+            % freeze_data(mouse_count, :) = DLC_data_mouse.was_freezing(1:21590);
             experimental_grps_updated(mouse_count, :) = experimental_grps(row_idx, :);
         end
     end
@@ -602,4 +604,272 @@ mean_data_array = {experimental_data, one_context_data, no_shock_data};
 sem_data_array = {experimental_sems_mice, one_context_sems_mice, no_shock_sems_mice};
 % need to make sure consec_thresh in perm_and_bCI is set to 10!
 [comparison, perm_p_sig] = perm_and_bCI_fn_analysis_PhilDBressel_for_1p(mean_data_array, sem_data_array, ts1);
+
+
+%% discrimination sessions
+clear aversive_context safe_context mean_aversive mean_safe mean_aversive_context mean_safe_context
+
+recorded_fps = 30;
+
+
+
+% Parameters
+stimulus_duration = 2 * 60; % 2 minutes in seconds
+num_repeats = 3;
+total_stimuli = 2;
+
+% Initialize variables
+stimulus_times = cell(total_stimuli, 1);
+current_time = 0;
+
+% Loop through each stimulus alternately and calculate start and end times
+for j = 1:num_repeats
+    for i = 1:total_stimuli
+        start_time = current_time;
+        end_time = start_time + stimulus_duration;
+        
+        if isempty(stimulus_times{i})
+            stimulus_times{i} = [start_time, end_time];
+        else
+            stimulus_times{i} = [stimulus_times{i}; start_time, end_time];
+        end
+        
+        current_time = end_time;
+    end
+end
+
+
+% Multiply every value in stimulus_times by the FPS
+for i = 1:total_stimuli
+    stimulus_frames{i} = stimulus_times{i} * recorded_fps;
+end
+
+
+
+% experimental_grps = readtable('E:\MATLAB\my_repo\context fear\organize_SLEAP_data\full_pilot_mice.xlsx');
+experimental_grps = readtable('I:\MATLAB\my_repo\context fear\organize_DLC_data\pilot groups.xlsx');
+
+% Define parameters
+threshold = 1; % Velocity threshold
+sample_duration = 0.03; % Duration of each sample in seconds
+min_duration = 2; % Minimum duration to trigger labeling in seconds
+frame_rate = 30; % Frames per second
+% Calculate the minimum number of consecutive rows needed
+min_samples = min_duration / sample_duration;
+
+animalIDs = fieldnames(final_DLC);
+
+session_to_analyze = 'D3';
+
+mouse_count = 0;
+for gg = 1:size(animalIDs, 1)
+    current_mouse = animalIDs{gg};
+    if strcmp(session_to_analyze, 'D1_Morning') & strcmp(current_mouse, 'B57417')
+        continue
+    else
+        if isfield(final_DLC.(current_mouse), session_to_analyze)
+            mouse_count = mouse_count+1;
+            DLC_data_mouse = final_DLC.(current_mouse).(session_to_analyze).movement_data;
+
+            body_velocity = [];
+            labels = [];
+            % Get the body_velocity column
+            body_velocity = final_DLC.(current_mouse).(session_to_analyze).movement_data.body_velocity;
+
+            % Initialize the new column
+            labels = zeros(size(body_velocity));
+
+            % Find consecutive segments where body_velocity < threshold
+            below_threshold = body_velocity < threshold;
+            start_idx = find(diff([0; below_threshold]) == 1); % Start indices
+            end_idx = find(diff([below_threshold; 0]) == -1); % End indices
+
+            % Iterate through each segment and label
+            for i = 1:length(start_idx)
+                segment_length = end_idx(i) - start_idx(i) + 1;
+                if segment_length >= min_samples
+                    labels(start_idx(i):end_idx(i)) = 1;
+                end
+            end
+
+            % Add the labels as a new column to the table
+            % final_DLC.B46837.D1_Afternoon.movement_data.freeze_label = labels;
+
+            % Find the row index where the 'mouse' column matches 'current_mouse'
+            row_idx = strcmp(experimental_grps.mouse, current_mouse);
+
+            freeze_data(mouse_count, :) = labels(1:21590)';
+            % freeze_data(mouse_count, :) = DLC_data_mouse.was_freezing(1:21590)';
+            experimental_grps_updated(mouse_count, :) = experimental_grps(row_idx, :);
+        end
+    end
+end
+
+
+
+if strcmp(session_to_analyze, 'D3')
+    for gg = 1:size(freeze_data, 1)
+        freeze_data_session = freeze_data(gg, :);
+
+        for qq = 1:num_repeats
+            safe_context{gg, qq} = freeze_data_session(stimulus_frames{1, 1}(qq,1)+1:stimulus_frames{1, 1}(qq,2));
+        end
+
+        for qq = 1:num_repeats
+            if qq <= 2
+                aversive_context{gg, qq} = freeze_data_session(stimulus_frames{1, 2}(qq,1)+1:stimulus_frames{1, 2}(qq,2));
+            elseif qq > 2
+                aversive_context{gg, qq} = freeze_data_session(stimulus_frames{1, 2}(qq,1)+1:end);
+            end
+        end
+    end
+
+elseif strcmp(session_to_analyze, 'D4')
+
+    for gg = 1:size(freeze_data, 1)
+        freeze_data_session = freeze_data(gg, :);
+
+        for qq = 1:num_repeats
+            aversive_context{gg, qq} = freeze_data_session(stimulus_frames{1, 1}(qq,1)+1:stimulus_frames{1, 1}(qq,2));
+        end
+
+        for qq = 1:num_repeats
+            if qq <= 2
+                safe_context{gg, qq} = freeze_data_session(stimulus_frames{1, 2}(qq,1)+1:stimulus_frames{1, 2}(qq,2));
+            elseif qq > 2
+                safe_context{gg, qq} = freeze_data_session(stimulus_frames{1, 2}(qq,1)+1:end);
+            end
+        end
+    end
+end
+
+
+
+
+% for gg = 1:size(freeze_data, 1)
+%     freeze_data_session = freeze_data(gg, :);
+% 
+%     for qq = 1:num_repeats
+%         safe_context{gg, qq} = freeze_data_session(stimulus_frames{1, 1}(qq,1)+1:stimulus_frames{1, 1}(qq,2));
+%     end
+% 
+%     for qq = 1:num_repeats
+%         if qq <= 2
+%             aversive_context{gg, qq} = freeze_data_session(stimulus_frames{1, 2}(qq,1)+1:stimulus_frames{1, 2}(qq,2));
+%         elseif qq > 2
+%             aversive_context{gg, qq} = freeze_data_session(stimulus_frames{1, 2}(qq,1)+1:end);
+%         end
+%     end
+% end
+
+for pp = 1:size(safe_context, 1)
+    for ff = 1:size(safe_context, 2)
+        % mean_safe_context(mouse_count, pp) = sum(freeze_data(stimulus_frames{1, 1}(pp,1)+1:stimulus_frames{1, 1}(pp,2)))/size(safe_context, 1);
+        % Calculate the proportion of 1s in the column
+        b = sum(safe_context{pp,ff}) / size(safe_context{pp,ff}, 2);
+
+        % Store the proportion in mean_aversive_context
+        mean_safe_context(pp, ff) = b;
+
+        sem_safe_context(pp, ff) = b/sqrt(size(safe_context{pp,ff}, 2));
+
+        % % Calculate the number of rows in the column
+        % n = size(safe_context{pp,ff}, 2);
+        % 
+        % % Calculate the standard error of the proportion
+        % standard_error_safe(pp, ff) = sqrt(b * (1 - b) / n);
+    end
+end
+
+
+for pp = 1:size(aversive_context, 1)
+    for ff = 1:size(aversive_context, 2)
+        % mean_safe_context(mouse_count, pp) = sum(freeze_data(stimulus_frames{1, 1}(pp,1)+1:stimulus_frames{1, 1}(pp,2)))/size(safe_context, 1);
+        % Calculate the proportion of 1s in the column
+        b = sum(aversive_context{pp,ff}) / size(aversive_context{pp,ff}, 2);
+
+        % Store the proportion in mean_aversive_context
+        mean_aversive_context(pp, ff) = b;
+
+        sem_aversive_context(pp, ff) = b/sqrt(size(aversive_context{pp,ff}, 2));
+
+        % % Calculate the number of rows in the column
+        % n = size(aversive_context{pp,ff}, 2);
+        % 
+        % % Calculate the standard error of the proportion
+        % standard_error_safe(pp, ff) = sqrt(b * (1 - b) / n);
+    end
+end
+
+
+experimental_data_safe = mean_safe_context(strcmp(experimental_grps_updated.group, 'Experimental'), :);
+experimental_mean_safe = mean(experimental_data_safe);
+experimental_sem_safe = std(experimental_data_safe)/sqrt(size(experimental_data_safe, 1));
+experimental_mice_safe = experimental_grps_updated(strcmp(experimental_grps_updated.group, 'Experimental'), :);
+
+one_context_data_safe = mean_safe_context(strcmp(experimental_grps_updated.group, 'One Context'), :);
+one_context_mean_safe = mean(one_context_data_safe);
+one_context_sem_safe = std(one_context_data_safe)/sqrt(size(one_context_data_safe, 1));
+one_context_mice_safe = experimental_grps_updated(strcmp(experimental_grps_updated.group, 'One Context'), :);
+
+no_shock_data_safe = mean_safe_context(strcmp(experimental_grps_updated.group, 'No Shock'), :);
+no_shock_mean_safe = mean(no_shock_data_safe);
+no_shock_sem_safe = std(no_shock_data_safe)/sqrt(size(no_shock_data_safe, 1));
+no_shock_mice_safe = experimental_grps_updated(strcmp(experimental_grps_updated.group, 'No Shock'), :);
+
+experimental_data_aversive = mean_aversive_context(strcmp(experimental_grps_updated.group, 'Experimental'), :);
+experimental_mean_aversive = mean(experimental_data_aversive);
+experimental_sem_aversive = std(experimental_data_aversive)/sqrt(size(experimental_data_aversive, 1));
+experimental_mice_aversive = experimental_grps_updated(strcmp(experimental_grps_updated.group, 'Experimental'), :);
+
+one_context_data_aversive = mean_aversive_context(strcmp(experimental_grps_updated.group, 'One Context'), :);
+one_context_mean_aversive = mean(one_context_data_aversive);
+one_context_sem_aversive = std(one_context_data_aversive)/sqrt(size(one_context_data_aversive, 1));
+one_context_mice_aversive = experimental_grps_updated(strcmp(experimental_grps_updated.group, 'One Context'), :);
+
+no_shock_data_aversive = mean_aversive_context(strcmp(experimental_grps_updated.group, 'No Shock'), :);
+no_shock_mean_aversive = mean(no_shock_data_aversive);
+no_shock_sem_aversive = std(no_shock_data_aversive)/sqrt(size(no_shock_data_aversive, 1));
+no_shock_mice_aversive = experimental_grps_updated(strcmp(experimental_grps_updated.group, 'No Shock'), :);    
+
+%%
+
+if strcmp(session_to_analyze, 'D3')
+    interleave_mean_experimental = zeros(size(experimental_data_safe, 1), 6);
+    interleave_mean_experimental(:, [1 3 5]) = experimental_data_safe;
+    interleave_mean_experimental(:, [2 4 6]) = experimental_data_aversive;
+
+    interleave_mean_one_context = zeros(size(one_context_data_safe, 1), 6);
+    interleave_mean_one_context(:, [1 3 5]) = one_context_data_safe;
+    interleave_mean_one_context(:, [2 4 6]) = one_context_data_aversive;
+
+    interleave_mean_no_shock = zeros(size(no_shock_data_safe, 1), 6);
+    interleave_mean_no_shock(:, [1 3 5]) = no_shock_data_safe;
+    interleave_mean_no_shock(:, [2 4 6]) = no_shock_data_aversive;
+    % interleave_sem_experimental = zeros(size(mean_safe_context, 1), 6);
+    % interleave_sem_experimental(:, [1 3 5]) = standard_error_safe;
+    % interleave_sem_experimental(:, [2 4 6]) = standard_error_aversive;
+
+
+
+elseif strcmp(session_to_analyze, 'D4')
+    interleave_mean_experimental = zeros(size(experimental_data_safe, 1), 6);
+    interleave_mean_experimental(:, [1 3 5]) = experimental_data_aversive;
+    interleave_mean_experimental(:, [2 4 6]) = experimental_data_safe;
+
+    interleave_mean_one_context = zeros(size(one_context_data_safe, 1), 6);
+    interleave_mean_one_context(:, [1 3 5]) = one_context_data_aversive;
+    interleave_mean_one_context(:, [2 4 6]) = one_context_data_safe;
+
+    interleave_mean_no_shock = zeros(size(no_shock_data_safe, 1), 6);
+    interleave_mean_no_shock(:, [1 3 5]) = no_shock_data_aversive;
+    interleave_mean_no_shock(:, [2 4 6]) = no_shock_data_safe;
+    % interleave_sem_experimental = zeros(size(mean_safe_context, 1), 6);
+    % interleave_sem_experimental(:, [1 3 5]) = standard_error_safe;
+    % interleave_sem_experimental(:, [2 4 6]) = standard_error_aversive;
+end
+figure; shadedErrorBar(1:size(mean(interleave_mean_experimental), 2), mean(interleave_mean_experimental), std(interleave_mean_experimental));
+hold on; shadedErrorBar(1:size(mean(interleave_mean_one_context), 2), mean(interleave_mean_one_context), std(interleave_mean_one_context));
+hold on; shadedErrorBar(1:size(mean(interleave_mean_no_shock), 2), mean(interleave_mean_no_shock), std(interleave_mean_no_shock));
+hold off; 
 
