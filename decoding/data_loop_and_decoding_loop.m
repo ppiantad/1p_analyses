53089ea5fed2ede510f2f470f59332bbab85b312
ppiantad <ptpiantadosi@smcm.edu>
2025-03-29 00:36:33 -0400

    updates to batch decoding

--- decoding/data_loop_and_decoding_loop.m
+++ decoding/data_loop_and_decoding_loop.m
@@ -11,7 +11,7 @@ ca_data_type = "C_raw"; % C % C_raw
 % CNMFe_data.C: denoised CNMFe traces
 % CNMFe_data.S: inferred spikes
 
-session_to_analyze = 'RDT_D1';
+session_to_analyze = 'Pre_RDT_RM';
 
 % these are mice that did not complete the entire session - kinda have to
 % toss them to do some comparisons during RDT
@@ -101,7 +101,7 @@ for dd = 1:size(neuron_subgroup, 1)
                         currentanimal = char(animalIDs(ii));
                         if isfield(final.(currentanimal), session_to_analyze)
                             BehavData = final.(currentanimal).(session_to_analyze).uv.BehavData;
-                            [BehavData,trials,varargin]=TrialFilter(BehavData,'OMITALL', 0, 'BLANK_TOUCH', 0, 'BLOCK', 1); %'OMITALL', 0, 'BLANK_TOUCH', 0
+                            [BehavData,trials,varargin]=TrialFilter(BehavData,'OMITALL', 0, 'BLANK_TOUCH', 0); %'OMITALL', 0, 'BLANK_TOUCH', 0
                             trials = cell2mat(trials);
                             ca = final.(currentanimal).(session_to_analyze).CNMFe_data.(ca_data_type);
 
@@ -173,7 +173,7 @@ for dd = 1:size(neuron_subgroup, 1)
                         currentanimal = char(animalIDs(ii));
                         if isfield(final.(currentanimal), session_to_analyze)
                             BehavData = final.(currentanimal).(session_to_analyze).uv.BehavData;
-                            [BehavData,trials,varargin]=TrialFilter(BehavData,'OMITALL', 0, 'BLANK_TOUCH', 0, 'BLOCK', 1);
+                            [BehavData,trials,varargin]=TrialFilter(BehavData,'OMITALL', 0, 'BLANK_TOUCH', 0);
                             trials = cell2mat(trials);
 
                             ca = final.(currentanimal).(session_to_analyze).CNMFe_data.(ca_data_type);
@@ -261,7 +261,7 @@ for dd = 1:size(neuron_subgroup, 1)
                         currentanimal = char(animalIDs(ii));
                         if isfield(final.(currentanimal), session_to_analyze)
                             BehavData = final.(currentanimal).(session_to_analyze).uv.BehavData;
-                            [BehavData,trials,varargin]=TrialFilter(BehavData,'OMITALL', 0, 'BLANK_TOUCH', 0, 'BLOCK', 1);
+                            [BehavData,trials,varargin]=TrialFilter(BehavData,'OMITALL', 0, 'BLANK_TOUCH', 0);
                             trials = cell2mat(trials);
 
                             ca = final.(currentanimal).(session_to_analyze).CNMFe_data.(ca_data_type);
@@ -344,7 +344,7 @@ for dd = 1:size(neuron_subgroup, 1)
             zall_mouse_iterations(1, num_iteration) = {zall_mouse};
         end
     end
-    data_for_decoding = caTraceTrials_mouse_iterations;
+    data_for_decoding = zall_mouse_iterations;
 
     %% only run this section if you want to not decode across time!
     % attempting to focus decoding on particular epoch. this section takes the mean activity in a "relevant_period" (pre-choice, etc) for each neuron to be decoded, plus its shuffle.
@@ -443,7 +443,11 @@ for dd = 1:size(neuron_subgroup, 1)
                 y = y(idx, :);
                 cv = cvpartition(size(X, 1),"KFold", k);
 
-
+                accuracy = zeros(k,1);
+                precision = zeros(k,1);
+                recall = zeros(k,1);
+                f1_score = zeros(k,1);
+                
                 for i = 1:k
                     xTrain = X(cv.training(i),:);
                     yTrain = y(cv.training(i),:);
@@ -451,63 +455,104 @@ for dd = 1:size(neuron_subgroup, 1)
                     yTest = y(cv.test(i), :);
                     % model = TreeBagger(numTrees, xTrain, yTrain, 'Method', 'classification');
                     % model = fitglm(xTrain, yTrain, 'Distribution', 'binomial' , 'Link', 'logit');
-                    % model = fitcsvm(xTrain, yTrain);
-                    model = fitcnb(xTrain, yTrain);
+                    model = fitcsvm(xTrain, yTrain);
+                    % model = fitcnb(xTrain, yTrain);
                     yPred = predict(model,xTest);
-                    accuracy(i) = sum(yPred == yTest)/numel(yTest);
+                    % Compute confusion matrix components
+                    TP = sum((yPred == 1) & (yTest == 1));
+                    TN = sum((yPred == 0) & (yTest == 0));
+                    FP = sum((yPred == 1) & (yTest == 0));
+                    FN = sum((yPred == 0) & (yTest == 1));
+
+                    % Calculate metrics
+                    accuracy(i) = (TP + TN) / (TP + TN + FP + FN);
+                    if (TP + FP) > 0
+                        precision(i) = TP / (TP + FP);
+                    else
+                        precision(i) = NaN;
+                    end
+                    if (TP + FN) > 0
+                        recall(i) = TP / (TP + FN);
+                    else
+                        recall(i) = NaN;
+                    end
+                    if (precision(i) + recall(i)) > 0
+                        f1_score(i) = 2 * (precision(i) * recall(i)) / (precision(i) + recall(i));
+                    else
+                        f1_score(i) = NaN;
+                    end
                 end
-                accuracy_by_offset(p) = mean(accuracy);
+
+                % Store averaged results across folds
+                accuracy_by_offset(p) = mean(accuracy, 'omitnan');
+                precision_by_offset(p) = mean(precision, 'omitnan');
+                recall_by_offset(p) = mean(recall, 'omitnan');
+                f1_score_by_offset(p) = mean(f1_score, 'omitnan');
             end
 
-            % figure; plot(ts1, accuracy_by_offset);
             accuracy_at_loop(:, bb) = accuracy_by_offset;
+            precision_at_loop(:, bb) = precision_by_offset;
+            recall_at_loop(:, bb) = recall_by_offset;
+            f1_score_at_loop(:, bb) = f1_score_by_offset;
         end
-        accuracy_per_iteration{iter}(uu) = {accuracy_at_loop};
-        cross_mouse_accuracy_per_iteration{iter}(:, uu) = mean(accuracy_at_loop, 2);
-        sem_accuracy_per_iteration{iter}(:, uu) = std(accuracy_at_loop,[],2)/sqrt(size(accuracy_at_loop, 2));
 
-        clear accuracy_at_loop
+        accuracy_per_iteration{iter}(uu) = {accuracy_at_loop};
+        precision_per_iteration{iter}(uu) = {precision_at_loop};
+        recall_per_iteration{iter}(uu) = {recall_at_loop};
+        f1_score_per_iteration{iter}(uu) = {f1_score_at_loop};
+
+        cross_mouse_accuracy_per_iteration{iter}(:, uu) = mean(accuracy_at_loop, 2);
+        cross_mouse_precision_per_iteration{iter}(:, uu) = mean(precision_at_loop, 2);
+        cross_mouse_recall_per_iteration{iter}(:, uu) = mean(recall_at_loop, 2);
+        cross_mouse_f1_per_iteration{iter}(:, uu) = mean(f1_score_at_loop, 2);
+
+        sem_accuracy_per_iteration{iter}(:, uu) = std(accuracy_at_loop,[],2)/sqrt(size(accuracy_at_loop, 2));
+        sem_precision_per_iteration{iter}(:, uu) = std(precision_at_loop,[],2)/sqrt(size(precision_at_loop, 2));
+        sem_recall_per_iteration{iter}(:, uu) = std(recall_at_loop,[],2)/sqrt(size(recall_at_loop, 2));
+        sem_f1_per_iteration{iter}(:, uu) = std(f1_score_at_loop,[],2)/sqrt(size(f1_score_at_loop, 2));
+
+        clear accuracy_at_loop precision_at_loop recall_at_loop f1_score_at_loop
     end
 
 end
 
 
 %% plot figure - specifics need to be tailored to decoding above
-for zz = 1:size(accuracy_per_iteration, 2)
-    current_iter = accuracy_per_iteration{1, zz}
-    for qq = 1:size(current_iter, 2)
-        sem_data{zz}(:, qq) = std(current_iter{1, qq}, [], 2)/sqrt(size(current_iter{1, qq},2));
-    end
-end
-
-
-[concatenatedTable_all] = get_median_choice_and_collect_fn(behav_tbl_iter)
-for zz = 1:size(concatenatedTable_all, 2)
-    temp_table = concatenatedTable_all{zz}; 
-    % median_choice_time_all(zz) = median(temp_table.choiceTime - temp_table.stTime);
-    median_collect_time_all(zz) = median(temp_table.collectionTime - temp_table.choiceTime);
-    median_start_time_all(zz) = median(temp_table.stTime - temp_table.choiceTime);
-    % median_choice_time_block_2_all(zz) = median(temp_table.choiceTime(temp_table.Block == 2) - temp_table.stTime(temp_table.Block == 2));
-    % median_choice_time_block_3_all(zz) = median(temp_table.choiceTime(temp_table.Block == 3) - temp_table.stTime(temp_table.Block == 3));
-    clear temp_table
-end
-
-
-
-
-figure;
-shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 1}, 2), nanmean(sem_data{1, 1}, 2), 'lineProps', {'color', 'k'});
-hold on;shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 2}, 2), nanmean(sem_data{1, 2}, 2), 'lineProps', {'color', 'g'});
-hold on;shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 3}, 2), nanmean(sem_data{1, 3}, 2), 'lineProps', {'color', 'c'});
-hold on;shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 4}, 2), nanmean(sem_data{1, 4}, 2), 'lineProps', {'color', 'b'});
-hold on;shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 5}, 2), nanmean(sem_data{1, 5}, 2), 'lineProps', {'color', [.7 .7 .7]});
-
-
-xline(0);
-xline(median_start_time_all, 'g', {'Median', 'start', 'time'})
-xline(median_collect_time_all, 'r', {'Median', 'collect', 'latency'})
-xlabel('Time from Large Rew Choice (s)');
-legend({'all neurons', 'pre-choice active', 'post-choice reward active', 'consumption active', 'shuffle (consumption shuff vs. consumption shuff)'}, 'Location','northwest')
+% for zz = 1:size(accuracy_per_iteration, 2)
+%     current_iter = accuracy_per_iteration{1, zz}
+%     for qq = 1:size(current_iter, 2)
+%         sem_data{zz}(:, qq) = std(current_iter{1, qq}, [], 2)/sqrt(size(current_iter{1, qq},2));
+%     end
+% end
+% 
+% 
+% [concatenatedTable_all] = get_median_choice_and_collect_fn(behav_tbl_iter)
+% for zz = 1:size(concatenatedTable_all, 2)
+%     temp_table = concatenatedTable_all{zz}; 
+%     % median_choice_time_all(zz) = median(temp_table.choiceTime - temp_table.stTime);
+%     median_collect_time_all(zz) = median(temp_table.collectionTime - temp_table.choiceTime);
+%     median_start_time_all(zz) = median(temp_table.stTime - temp_table.choiceTime);
+%     % median_choice_time_block_2_all(zz) = median(temp_table.choiceTime(temp_table.Block == 2) - temp_table.stTime(temp_table.Block == 2));
+%     % median_choice_time_block_3_all(zz) = median(temp_table.choiceTime(temp_table.Block == 3) - temp_table.stTime(temp_table.Block == 3));
+%     clear temp_table
+% end
+% 
+% 
+% 
+% 
+% figure;
+% shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 1}, 2), nanmean(sem_data{1, 1}, 2), 'lineProps', {'color', 'k'});
+% hold on;shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 2}, 2), nanmean(sem_data{1, 2}, 2), 'lineProps', {'color', 'g'});
+% hold on;shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 3}, 2), nanmean(sem_data{1, 3}, 2), 'lineProps', {'color', 'c'});
+% hold on;shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 4}, 2), nanmean(sem_data{1, 4}, 2), 'lineProps', {'color', 'b'});
+% hold on;shadedErrorBar(ts1, nanmean(cross_mouse_accuracy_per_iteration{1, 5}, 2), nanmean(sem_data{1, 5}, 2), 'lineProps', {'color', [.7 .7 .7]});
+% 
+% 
+% xline(0);
+% xline(median_start_time_all, 'g', {'Median', 'start', 'time'})
+% xline(median_collect_time_all, 'r', {'Median', 'collect', 'latency'})
+% xlabel('Time from Large Rew Choice (s)');
+% legend({'all neurons', 'pre-choice active', 'post-choice reward active', 'consumption active', 'shuffle (consumption shuff vs. consumption shuff)'}, 'Location','northwest')
 
 %%
 % Initialize variables to store means and standard deviations for each group
@@ -568,3 +613,120 @@ yticklabels(flip(group_labels)); % Correct order of labels
 xlim([0.5 1])
 xticks([0.5 0.75 1])
 legend('Pre-choice epoch', 'Post-choice epoch', 'Consumption epoch', 'Shuffle (Consumption)', 'Location', 'eastoutside');
+
+
+%%
+
+% Initialize vectors to store results
+num_pairs = 4; % Since you have 8 cells, there are 4 pairs
+h_values = zeros(1, num_pairs);
+p_values = zeros(1, num_pairs);
+
+for i = 1:num_pairs
+    % Get the index for the paired cells
+    idx1 = (i - 1) * 2 + 1;
+    idx2 = idx1 + 1;
+    
+    % Convert each cell to a matrix and compute mean accuracy
+    data_matrix_1 = cell2mat(accuracy_per_iteration{1, idx1}');
+    mean_accuracy_1 = mean(data_matrix_1, 1);
+    
+    data_matrix_2 = cell2mat(accuracy_per_iteration{1, idx2}');
+    mean_accuracy_2 = mean(data_matrix_2, 1);
+    
+    % Perform paired-samples t-test
+    [h, p] = ttest(mean_accuracy_1, mean_accuracy_2);
+    
+    % Store results
+    h_values(i) = h;
+    p_values(i) = p;
+end
+
+% Display results
+disp('Paired t-test results:');
+disp(table((1:num_pairs)', h_values', p_values', 'VariableNames', {'Pair', 'h', 'p'}));
+
+%%
+% Define figure width and height (adjust as needed)
+fig_width = 300; % Width in pixels
+fig_height = 600; % Height in pixels
+
+% Create figure with custom size
+figure('Position', [100, 100, fig_width, fig_height]); 
+hold on;
+
+% Number of pairs
+num_pairs = 4; 
+mean_values = zeros(num_pairs, 2); % Store means for each pair
+sem_values = zeros(num_pairs, 2); % Store SEM for each pair
+all_data = cell(num_pairs, 2); % Store individual data for scatter
+
+% Process data for plotting
+for i = 1:num_pairs
+    idx1 = (i - 1) * 2 + 1;
+    idx2 = idx1 + 1;
+    
+    % Convert cell contents to matrices and compute means
+    data_matrix_1 = cell2mat(f1_score_per_iteration{1, idx1}');
+    data_matrix_2 = cell2mat(f1_score_per_iteration{1, idx2}');
+    
+    % Mean values
+    mean_values(i, 1) = mean(mean(data_matrix_1, 1));
+    mean_values(i, 2) = mean(mean(data_matrix_2, 1));
+    
+    % Standard Error of the Mean (SEM)
+    sem_values(i, 1) = std(mean(data_matrix_1, 1)) / sqrt(size(data_matrix_1, 1));
+    sem_values(i, 2) = std(mean(data_matrix_2, 1)) / sqrt(size(data_matrix_2, 1));
+    
+    % Store data for scatter plot
+    all_data{i, 1} = mean(data_matrix_1, 1);
+    all_data{i, 2} = mean(data_matrix_2, 1);
+end
+
+% Reverse order of bars
+mean_values = flipud(mean_values);
+sem_values = flipud(sem_values);
+all_data = flipud(all_data);
+
+% Create horizontal bar plot
+y_positions = 1:num_pairs; % Set positions for the pairs
+bar_width = 0.6; % Bar width to allow space for scatter points
+
+% Plot bars
+bar_handles = barh(y_positions, mean_values, bar_width, 'grouped');
+bar_handles(1).FaceColor = [0.2, 0.4, 0.8]; % Color for first condition
+bar_handles(2).FaceColor = [0.8, 0.4, 0.2]; % Color for second condition
+
+% Get the actual bar positions
+x_offsets = [bar_handles(1).XEndPoints; bar_handles(2).XEndPoints];
+
+% Add error bars centered on bars
+errorbar(mean_values(:,1), x_offsets(1,:), sem_values(:,1), 'horizontal', 'k', 'linestyle', 'none', 'linewidth', 1.5);
+errorbar(mean_values(:,2), x_offsets(2,:), sem_values(:,2), 'horizontal', 'k', 'linestyle', 'none', 'linewidth', 1.5);
+
+% Add scatter points & connect them
+for i = 1:num_pairs
+    % Get scatter y-positions directly over bars
+    y1 = x_offsets(1, i) * ones(size(all_data{i, 1}));
+    y2 = x_offsets(2, i) * ones(size(all_data{i, 2}));
+    
+    % Scatter individual points
+    scatter(all_data{i, 1}, y1, 50, 'b', 'filled', 'MarkerFaceAlpha', 0.6);
+    scatter(all_data{i, 2}, y2, 50, 'r', 'filled', 'MarkerFaceAlpha', 0.6);
+    
+    % Connect points with lines
+    plot([all_data{i, 1}; all_data{i, 2}], [y1; y2], 'k-', 'LineWidth', 1);
+end
+
+% Labels & Aesthetics
+yticks(y_positions);
+yticklabels({'Pair 4', 'Pair 3', 'Pair 2', 'Pair 1'}); % Reverse order
+% xlabel('Mean Accuracy');
+% title('Paired Accuracy Comparisons');
+% legend({'Condition 1', 'Condition 2'}, 'Location', 'SouthEast');
+set(gca, 'FontSize', 12);
+% Set X-axis limits and define ticks explicitly
+xlim([0.35, 1]); 
+xticks(0.4:0.1:1); % Tick marks at each value from 0.4 to 0.9
+box off;
+hold off;
